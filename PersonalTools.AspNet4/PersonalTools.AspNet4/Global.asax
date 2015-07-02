48a72757dec6794daa19cbfb869b508b6c62a2a4
mingezhao <mingezhao@gmail.com>
2015-07-02 22:57:21 +0800

    Create solution.

---
+++ PersonalTools.AspNet4/PersonalTools.AspNet4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PersonalTools.AspNet4.MvcApplication" Language="C#" %>
---
+++ PersonalTools.AspNet4/PersonalTools.AspNet4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PersonalTools.AspNet4.Startup))]
+namespace PersonalTools.AspNet4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

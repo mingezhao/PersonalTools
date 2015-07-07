ade7354b6190c38ca9f935a6ba546ffa13e6dd6c
mingezhao <mingezhao@gmail.com>
2015-07-07 09:03:05 +0800

    Temp submit.

--- PersonalTools.Core/PersonalTools.Data/Entities/UserEntity.cs
+++ PersonalTools.Core/PersonalTools.Data/Entities/UserEntity.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,7 @@ namespace PersonalTools.Data.Entities
 {
     public class UserEntity : IEntity
     {
+        [BsonId(IdGenerator = typeof(StringObjectIdGenerator))]
         public string Id { get; set; }
     }
 }
